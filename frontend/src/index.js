3e2c6b3d7fd309241666ea3e395a66a719e26a51
mesdevelop <155332540+mesdevelop@users.noreply.github.com>
2026-07-23 05:48:22 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/dashboard", element: <Dashboard /> }
+]);
+
+<RouterProvider router={router} />
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
